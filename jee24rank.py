baee446ddcfe1c417e295ebb372923df7cc550b1
Adirom0901 <159532875+Adirom0901@users.noreply.github.com>
2025-03-27 17:46:49 +0530

    Update jee24rank.py

--- jee24rank.py
+++ jee24rank.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load Data
-df_merged = pd.read_csv("C:/Users/aditya namdeo/Downloads/df_merged.csv")
+df_merged = pd.read_csv("df_merged.csv")
 
 # Visualization Function
 def visualize(Institute, Seat_Type, Gender):
